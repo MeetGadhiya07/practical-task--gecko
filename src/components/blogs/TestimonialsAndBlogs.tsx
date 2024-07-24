e921b9e2bca00fcb4c10f22ff5122f9574fa0901
Meet Gadhiya <mailto:meetgadhiya000@gmail.com>
2024-07-24 22:44:07 +0530

    Completed testimonials section design

--- src/components/blogs/TestimonialsAndBlogs.tsx
+++ src/components/blogs/TestimonialsAndBlogs.tsx
@@ -1,17 +1,81 @@
-import React from "react";
+import profile from "../../assets/Images/josh-man.png";
+import topQuote from "../../assets/Images/top-quote.svg";
+import downQuote from "../../assets/Images/down-quote.svg";
 
 const TestimonialsAndBlogs = () => {
-  return (
-    <section className="h-[1277px] ">
-      <div className="container bg-[url('/src/assets/Images/testimonial.svg')] h-full">
-        <section className=" ">
-          <h1 className="text-secondary text-[47px] leading-[58.99px] font-bold  mb-20  pt-[87.12px]">
-            Our Process
-          </h1>
+    return (
+        <section className="h-[1277px] ">
+            <div className="container bg-[url('/src/assets/Images/testimonial.svg')] h-full">
+                <section className=" ">
+                    <h1 className="text-secondary text-[47px] leading-[58.99px] font-bold  mb-20  pt-[87.12px]">
+                        Testimonials
+                    </h1>
+
+                    <div className="w-full flex items-center justify-center gap-[98px]">
+                        <div className="bg-white w-[502px] rounded-xl p-8 shadow relative">
+                            <img
+                                src={topQuote}
+                                alt="top-quote"
+                                className="absolute -top-[17px] left-[10px]"
+                            ></img>
+                            <div className="flex justify-start items-center mb-3">
+                                <img
+                                    src={profile}
+                                    alt="profile"
+                                    className="rounded-full mr-4"
+                                ></img>
+                                <p className="text-lg font-bold ">
+                                    Josh brollins
+                                </p>
+                            </div>
+                            <p className="text-[#5E5E5E] leading-8">
+                                Lorem ipsum dolor sit amet, consetetur
+                                sadipscing elitr, sed diam nonumy eirmod tempor
+                                invidunt ut labore et dolore magna aliquyam
+                                erat, sed diam voluptua
+                            </p>
+
+                            <img
+                                src={downQuote}
+                                alt="top-quote"
+                                className="absolute -bottom-[17px] right-[12px]"
+                            ></img>
+                        </div>
+
+                        <div className="bg-white w-[502px] rounded-xl p-8 shadow relative">
+                            <img
+                                src={topQuote}
+                                alt="top-quote"
+                                className="absolute -top-[17px] left-[10px]"
+                            ></img>
+                            <div className="flex justify-start items-center mb-3">
+                                <img
+                                    src={profile}
+                                    alt="profile"
+                                    className="rounded-full mr-4"
+                                ></img>
+                                <p className="text-lg font-bold ">
+                                    Josh brollins
+                                </p>
+                            </div>
+                            <p className="text-[#5E5E5E] leading-8">
+                                Lorem ipsum dolor sit amet, consetetur
+                                sadipscing elitr, sed diam nonumy eirmod tempor
+                                invidunt ut labore et dolore magna aliquyam
+                                erat, sed diam voluptua
+                            </p>
+
+                            <img
+                                src={downQuote}
+                                alt="top-quote"
+                                className="absolute -bottom-[17px] right-[12px]"
+                            ></img>
+                        </div>
+                    </div>
+                </section>
+            </div>
         </section>
-      </div>
-    </section>
-  );
+    );
 };
 
 export default TestimonialsAndBlogs;
